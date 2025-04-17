70cef35ae514acaaaedef141e605931fb9b89ef4
Greg Preiss <gregory.preiss@gmail.com>
2025-04-16 19:57:12 -0600

    added my server code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+require("dotenv").config();
+app.use(express.json());
+
+//  Static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).send("404 - Page Not Found");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
+});
